3d16c97d250b279295cee7531ef4f324bcc315bd
a-basuony <A.Basuony.social@gmail.com>
2024-01-19 20:46:40 +0200

    adding validation logic for register inputs

--- index.js
+++ index.js
@@ -1,28 +1,69 @@
 const form = document.getElementsByClassName("form")[0];
-const userNameInput = document.getElementById("name");
-const emailInput = document.getElementById("email");
-const passwordInput = document.getElementById("password");
-const confirmPasswordInput = document.getElementById("confirmPassword");
+let userNameInput = document.getElementById("name");
+let emailInput = document.getElementById("email");
+let passwordInput = document.getElementById("password");
+let confirmPasswordInput = document.getElementById("confirmPassword");
 
-// check if the input is required when focus changed
-function addRequiredValidation(input) {
-  input.addEventListener("focus", function () {
-    //if the field is empty, add an error class
-    if (input.value.trim() === "") {
-      input.parentNode.classList.add("error");
-    } else {
-      //otherwise remove the error class
-      input.classList.remove("error");
-    }
-  });
+//showError if the inputs not required
+function showError(input, message) {
+  let formControl = input.parentElement;
+  formControl.className = "form_control error";
+  let small = formControl.querySelector("small");
+  small.innerText = message;
 }
 
-addRequiredValidation(userNameInput);
-addRequiredValidation(userNameInput);
-addRequiredValidation(emailInput);
-addRequiredValidation(passwordInput);
-addRequiredValidation(confirmPasswordInput);
+//showSuccess if the inputs required
+function showSuccess(input) {
+  let formControl = input.parentElement;
+  formControl.className = "form_control success";
+}
 
+// form submit event
 form.addEventListener("submit", function (e) {
   e.preventDefault();
+
+  if (userNameInput.value.trim() === "") {
+    showError(userNameInput, "Username is required");
+  } else {
+    showSuccess(userNameInput);
+  }
+
+  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (emailRegex.test(emailInput.value)) {
+    showSuccess(emailInput);
+  } else {
+    showError(emailInput, "Email is invalid");
+  }
+
+  if (
+    passwordInput.value.length >= 5 &&
+    passwordInput.value.length <= 16 &&
+    passwordInput.value.trim() !== ""
+  ) {
+    showSuccess(passwordInput);
+  } else {
+    showError(
+      passwordInput,
+      "Password must be between 8 and 16 characters long."
+    );
+  }
+
+  if (
+    confirmPasswordInput.value === passwordInput.value &&
+    confirmPasswordInput.value.trim() !== ""
+  ) {
+    showSuccess(confirmPasswordInput);
+  } else {
+    showError(confirmPasswordInput, "Passwords do not match");
+  }
+
+  console.log(userNameInput.value);
+  console.log(emailInput.value);
+  console.log(passwordInput.value);
+  console.log(confirmPasswordInput.value);
+
+  userNameInput.value = "";
+  emailInput.value = "";
+  passwordInput.value = "";
+  confirmPasswordInput.value = "";
 });
